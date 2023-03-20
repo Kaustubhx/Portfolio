9761ce094adf4fea1fe9994a701647a69cb15b49
Kastubhx <prime.ganekar@gmail.com>
2023-03-20 09:53:22 +0530

    Update & New: Sanity/Dummy Images

---
+++ sanity/schemas/index.js
@@ -0,0 +1,7 @@
+import pageInfo from './pageInfo'
+import experience from './experience'
+import projects from './projects'
+import skills from './skills'
+import social from './social'
+
+export const schemaTypes = [pageInfo, experience, projects, skills, social];
