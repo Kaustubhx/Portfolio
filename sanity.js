925d03e2f8319a0aceb16940d39d1c649b1f3d50
Kastubhx <prime.ganekar@gmail.com>
2023-03-21 02:03:53 +0530

    Fix: SanityID

--- sanity.js
+++ sanity.js
@@ -2,8 +2,8 @@ import { createClient, groq } from 'next-sanity'
 import createImageUrlBuilder from '@sanity/image-url'
 
 export const config = {
+    projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || 'ydtefbyj',
     dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
-    projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
     apiVersion: "2022-11-16",
     useCdn: false,
 }
