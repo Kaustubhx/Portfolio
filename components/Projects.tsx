0f97a3d476b401e1fd2a30fa156bf1cc031f7920
Kastubhx <prime.ganekar@gmail.com>
2023-11-14 23:32:31 +0530

    Fix: Missing Project Link

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -5,6 +5,7 @@ import { motion } from 'framer-motion'
 import Image from 'next/image';
 import { Project } from '@/typings';
 import { urlFor } from '@/sanity';
+import Link from 'next/link';
 
 type Props = {
     projects: Project[];
@@ -30,9 +31,12 @@ function Projects({ projects }: Props) {
                 <div className='mt-12 relative w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20 pb-20 scrollbar-thin scrollbar-track-neutral-900/20 scrollbar-thumb-neutral-900/80'>
                     {projects.map((project, i) => (
                         <div key={project?._id} className='w-screen flex-shrink-0 snap-center snap-mandatory flex flex-col space-y-5 items-center justify-center px-10'>
-                            <div className='h-56 w-56 md:h-64 md:w-64 lg:h-[300px] lg:w-[500px] relative'>
+                            <Link
+                                href={project?.linkToBuild}
+                                className='h-56 w-56 md:h-64 md:w-64 lg:h-[300px] lg:w-[500px] relative'
+                            >
                                 <Image className='object-contain' src={urlFor(project?.image).url()} alt='' fill />
-                            </div>
+                            </Link>
 
                             <div className='space-y-10 max-w-7xl'>
                                 <h4 className='text-3xl md:text-4xl font-semibold text-center'>
